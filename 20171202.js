4bb4cba898c03c029d813ce03d4603182abdb262
Viktor Jakobsson <vjakobsson@rapidimages.se>
2017-12-08 08:44:28 +0100

    completed part2 day2

--- 20171202.js
+++ 20171202.js
@@ -15,7 +15,9 @@ http.request(options, (response) => {
   })
 
   response.on('end', () => {
-    console.log(doCalculation(str))
+    console.log('part1', doCalculation(str))
+
+    console.log('part2', doCalculation2(str))
   })
 }).end()
 
@@ -29,5 +31,24 @@ function doCalculation (str) {
     }, { min: Infinity, max: -Infinity })
   }).reduce((memo, next, index, arr) => {
     return memo + (next.max - next.min)
+  }, 0)
+}
+
+function doCalculation2 (str) {
+  return str.split('\n').filter(Boolean).map((row) => {
+    return row.split('\t').reduce((memo, next, index, arr) => {
+      if (memo) return memo
+
+      return arr.reduce((memo2, next2, index2, arr2) => {
+        if ((index === index2) || memo2) return memo2
+
+        if ((next % next2) === 0) return next / next2
+        if ((next2 % next) === 0) return next2 / next
+
+        return memo2
+      }, NaN)
+    }, false)
+  }).reduce((memo, next, index, arr) => {
+    return memo + (next)
   }, 0)
 }
